2a300d134ea33624f830da49fa537458a2ab8a20
Kevin Ahn <maketheworldwise@gmail.com>
2023-01-16 22:34:30 +0900

    Feature: DB에 바로 반영되지 않는다?
    - DB에 바로 반영되는지 여부 확인
    - DB 옵션 수정

--- src/main/java/com/example/JpaMain.java
+++ src/main/java/com/example/JpaMain.java
@@ -16,42 +16,14 @@ public class JpaMain {
 		EntityTransaction entityTransaction = entityManager.getTransaction();
 		entityTransaction.begin();
 
-		// DB 데이터 CRUD 로직
 		try {
-			// [INSERT]
-			//
-			// Member member = new Member();
-			// member.setId(1L);
-			// member.setName("Hello");
-			//
-			// entityManager.persist(member);
+			Member member = new Member();
+			member.setId(1L);
+			member.setName("Hello");
 
-			// [SELECT]
-			//
-			Member findMember = entityManager.find(Member.class, 1L);
-			System.out.println("id: " + findMember.getId());
-			System.out.println("name: " + findMember.getName()); // Hello
-
-			// [DELETE]
-			//
-			// entityManager.remove(findMember);
-
-			// [UPDATE]
-			//
-			findMember.setName("World");
-			System.out.println("id: " + findMember.getId());
-			System.out.println("name: " + findMember.getName()); // World
-
-			// [JPQL]
-			// 테이블을 대상이 아닌 엔티티 객체를 대상으로 쿼리를 구성
-			List<Member> result = entityManager.createQuery("SELECT m from Member as m", Member.class)
-				.setFirstResult(1) // Paging 1 ~ 10 (limit, offset)
-				.setMaxResults(10)
-				.getResultList();
-
-			for(Member member : result) {
-				System.out.println("result: " + member.getName());
-			}
+			System.out.println("=== BEFORE ===");
+			entityManager.persist(member);
+			System.out.println("=== AFTER ===");
 
 			entityTransaction.commit();
 		} catch (Exception e) {
