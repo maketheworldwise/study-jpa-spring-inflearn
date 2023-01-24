938ce86149819d19e42214d68f294f4e6ef118ea
Kevin Ahn <maketheworldwise@gmail.com>
2023-01-24 14:45:42 +0900

    Feature: 일대일 단방향 연관관계 매핑

--- src/main/java/com/example/Member.java
+++ src/main/java/com/example/Member.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -28,6 +29,10 @@ public class Member {
 	// @ManyToOne
 	// @JoinColumn(name = "TEAM_ID")
 	// private Team team;
+
+	@OneToOne
+	@JoinColumn(name = "LOCKER_ID")
+	private Locker locker;
 
 	public Long getId() {
 		return id;
