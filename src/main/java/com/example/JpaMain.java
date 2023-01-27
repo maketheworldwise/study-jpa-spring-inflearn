7049550642f77fb03b169856d64cb940863cf6f8
Kevin Ahn <maketheworldwise@gmail.com>
2023-01-28 02:02:07 +0900

    Feature: 영속성 컨텍스트에 존재하는 실제 엔티티 객체 반환
    - 영속성 컨텍스트에 엔티티가 있을 경우 실제 엔티티 반환

--- src/main/java/com/example/JpaMain.java
+++ src/main/java/com/example/JpaMain.java
@@ -17,21 +17,18 @@ public class JpaMain {
 		entityTransaction.begin();
 
 		try {
-			Member member1 = new Member();
-			member1.setName("member1");
-			entityManager.persist(member1);
-
-			Member member2 = new Member();
-			member2.setName("member2");
-			entityManager.persist(member2);
+			Member member = new Member();
+			member.setName("member1");
+			entityManager.persist(member);
 
 		 	entityManager.flush();
 			entityManager.clear();
 
-			Member findMember1 = entityManager.find(Member.class, member1.getId());
-			Member findMember2 = entityManager.getReference(Member.class, member2.getId());
+			Member findMember1 = entityManager.find(Member.class, member.getId());
+			System.out.println("findMember1 : " + findMember1.getClass()); // Member
 
-			logic(findMember1, findMember2);
+			Member findMember2 = entityManager.getReference(Member.class, member.getId());
+			System.out.println("findMember2 : " + findMember2.getClass()); // 프록시 객체가 아닌 Member 출력
 
 			entityTransaction.commit();
 		} catch (Exception e) {
@@ -41,13 +38,5 @@ public class JpaMain {
 		}
 
 		entityManagerFactory.close();
-	}
-
-	private static void logic(Member m1, Member m2) {
-		// 여기 메서드에 넘어오는 값이 프록시 객체인지 실제 엔티티 객체인지 모름
-		System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass()));
-		// 따라서 instanceof를 이용하여 비교 필요
-		System.out.println("m1 instanceof Member : " + (m1 instanceof Member));
-		System.out.println("m2 instanceof Member : " + (m2 instanceof Member));
 	}
 }
