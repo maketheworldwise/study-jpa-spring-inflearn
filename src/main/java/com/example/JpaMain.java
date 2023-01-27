26ed2ba2cf591c932359d3f2a0aa8364132059e4
Kevin Ahn <maketheworldwise@gmail.com>
2023-01-28 02:42:57 +0900

    Feature: 프록시 유틸리티

--- src/main/java/com/example/JpaMain.java
+++ src/main/java/com/example/JpaMain.java
@@ -7,6 +7,8 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import org.hibernate.Hibernate;
+
 public class JpaMain {
 
 	public static void main(String[] args) {
@@ -27,11 +29,11 @@ public class JpaMain {
 			Member findMember1 = entityManager.getReference(Member.class, member.getId());
 			System.out.println("findMember1 : " + findMember1.getClass());
 
-			entityManager.detach(findMember1);
-			// entityManager.clear();
-			// entityManager.close();
+			System.out.println("Before isLoaded = " + entityManagerFactory.getPersistenceUnitUtil().isLoaded(findMember1));
+			System.out.println(findMember1.getName()); // 프록시 초기화
+			System.out.println("After isLoaded = " + entityManagerFactory.getPersistenceUnitUtil().isLoaded(findMember1));
 
-			System.out.println(findMember1.getName());
+			Hibernate.initialize(findMember1); // 강제 초기화
 
 			entityTransaction.commit();
 		} catch (Exception e) {
