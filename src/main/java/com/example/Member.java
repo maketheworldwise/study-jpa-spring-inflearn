07dcc06bd960892aba3e9c367519b344c6a9bfbd
Kevin Ahn <maketheworldwise@gmail.com>
2023-01-28 15:25:13 +0900

    Feature: 즉시 로딩

--- src/main/java/com/example/Member.java
+++ src/main/java/com/example/Member.java
@@ -29,7 +29,7 @@ public class Member extends BaseEntity {
 	@Column(name = "USERNAME")
 	private String name;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "TEAM_ID")
 	private Team team;
 
